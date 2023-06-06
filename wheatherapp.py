576407b0c0a688968aef10737e83d71afb11bab8
max <max@szperlinski.pl>
2023-06-06 20:49:20 +0200

    Script works, no more 401 error

--- wheatherapp.py
+++ wheatherapp.py
@@ -4,9 +4,12 @@ api_key = 'e8081ab0568ec8b77c4318ba87cece26'
 
 user_input = input("Enter the city name: ")
 
-weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}").json()
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={api_key}").json()
 
-print(weather_data)
+if weather_data['cod'] == '404':
+    print('City not found')
+else
+    weather = weather_data['weather'][0]['main']
+    temp = round(weather_data['main']['temp']) 
 
-
-test
+print(f'the weather in {user_input} is {weather} and the temperature is {temp} degrees Celcius.')
