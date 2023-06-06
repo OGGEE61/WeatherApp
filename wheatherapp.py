05106208de8a7d8805806270333183b97da21cea
max <max@szperlinski.pl>
2023-06-06 19:48:54 +0200

    edit

--- wheatherapp.py
+++ wheatherapp.py
@@ -4,5 +4,6 @@ api_key = 'e8081ab0568ec8b77c4318ba87cece26'
 
 user_input = input("Enter the city name: ")
 
-weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&APPID={api_key}").json()
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}").json()
+
 print(weather_data)
