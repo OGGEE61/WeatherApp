44bb9777d632620e84b6ff6d84cd0873b3c60987
max <max@szperlinski.pl>
2023-06-07 16:49:24 +0200

    Added the time of sunrise and sunset to the output

--- wheatherapp.py
+++ wheatherapp.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 
 api_key = 'e8081ab0568ec8b77c4318ba87cece26'
 
@@ -10,6 +11,14 @@ if weather_data['cod'] == '404':
     print('City not found')
 else:
     weather = weather_data['weather'][0]['main']
-    temp = round(weather_data['main']['temp']) 
+    temp = round(weather_data['main']['temp'])
+    sunrise = datetime.datetime.fromtimestamp(weather_data['sys']['sunrise'] )
+    sunset = datetime.datetime.fromtimestamp(weather_data['sys']['sunset'])
+    
+# Extract only the time portion
+sunrise_time = sunrise.time()
+sunset_time = sunset.time()
 
-print(f'the weather in {user_input} is {weather} and the temperature is {temp} degrees Celcius.')
+
+print(f'The weather in {user_input} is {weather} and the temperature is {temp} degrees Celcius.')
+print(f'Sunrise is at {sunrise_time} and sunset is at {sunset_time}.')
